417b4d08c74d35c17e8d34a4920ee27ce9c9297f
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-07-04 23:39:04 +0530

    Time: 119 ms (92.54%), Space: 16.9 MB (91.87%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.cpp
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    void backtrack(string &num, int target, int idx, long long value, long long last, string temp, vector<string>& result){
+    void backtrack(string &num, int target, int idx, long long value, long long last, string& temp, vector<string>& result){
+        // TC:- O(n^2*3^n)
         if(idx==num.size()) {
             if(value==target) result.push_back(temp);
             return;
@@ -9,19 +10,27 @@ public:
             string str=num.substr(idx, k-idx+1);
             if(k!=idx && num[idx] == '0') break; //invalid leading zero
             long long curr=stoll(str);
+            int len=temp.size();
             if(idx==0){
                 backtrack(num, target, k+1, curr, curr, str, result);
             }else{
-                backtrack(num, target, k+1, value+curr, curr, temp+'+'+str, result);
-                backtrack(num, target, k+1, value-curr, -curr, temp+'-'+str, result);
-                backtrack(num, target, k+1, value-last+last*curr, last*curr, temp+'*'+str, result);
+                temp+='+'+str;
+                backtrack(num, target, k+1, value+curr, curr, temp, result);
+                temp.resize(len);
+                temp+='-'+str;
+                backtrack(num, target, k+1, value-curr, -curr, temp, result);
+                temp.resize(len);
+                temp+='*'+str;
+                backtrack(num, target, k+1, value-last+last*curr, last*curr, temp, result);
+                temp.resize(len);
             }
         }
     }
 
     vector<string> addOperators(string num, int target) {
         vector<string> result;
-        backtrack(num, target, 0, 0, 0, "", result);
+        string temp="";
+        backtrack(num, target, 0, 0, 0, temp, result);
         return result;
     }
 };
