63719246c7b7998f2ce7c21029323c3f9676188c
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-07-07 18:37:05 +0530

    Time: 0 ms (100%), Space: 10 MB (60.24%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
-    void recur(vector<vector<int>>& result, vector<int>& nums, int idx, vector<int>& temp){
-        if(idx==nums.size()){
-            result.push_back(temp);
-            return;
+    // void recur(vector<vector<int>>& result, vector<int>& nums, int idx, vector<int>& temp){
+    //     if(idx==nums.size()){
+    //         result.push_back(temp);
+    //         return;
+    //     }
+    //     recur(result, nums, idx+1, temp);
+    //     temp.push_back(nums[idx]);
+    //     recur(result, nums, idx+1, temp);
+    //     temp.pop_back();
+    // }
+
+    void backtrack(vector<vector<int>>& result, vector<int>& nums, int idx, vector<int>& temp){
+        result.push_back(temp);
+        for(int i=idx; i<nums.size(); i++){
+            temp.push_back(nums[i]);
+            backtrack(result, nums, i+1, temp);
+            temp.pop_back();
         }
-        recur(result, nums, idx+1, temp);
-        temp.push_back(nums[idx]);
-        recur(result, nums, idx+1, temp);
-        temp.pop_back();
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
-        // TC:- O(n*2^n)
+        // Recursion TC:- O(n*2^n)
         // it will not generate duplicate sets because the numbers in nums are distinct
+        // vector<vector<int>> result;
+        // vector<int> temp;
+        // recur(result, nums, 0, temp);
+        // return result;
+
+        // Backtracking TC:- O(n*2^n)
+        // this approach is quite similar to recursion just difference in recursion tree made
         vector<vector<int>> result;
         vector<int> temp;
-        recur(result, nums, 0, temp);
+        backtrack(result, nums, 0, temp);
         return result;
     }
 };
