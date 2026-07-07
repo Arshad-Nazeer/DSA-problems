c8bffb7e9cdc54fca6899bb5936b0a27a4c5ed39
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-07-07 18:57:49 +0530

    Time: 0 ms (100%), Space: 10 MB (60.24%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -11,14 +11,14 @@ public:
     //     temp.pop_back();
     // }
 
-    // void backtrack(vector<vector<int>>& result, vector<int>& nums, int idx, vector<int>& temp){
-    //     result.push_back(temp);
-    //     for(int i=idx; i<nums.size(); i++){
-    //         temp.push_back(nums[i]);
-    //         backtrack(result, nums, i+1, temp);
-    //         temp.pop_back();
-    //     }
-    // }
+    void backtrack(vector<vector<int>>& result, vector<int>& nums, int idx, vector<int>& temp){
+        result.push_back(temp);
+        for(int i=idx; i<nums.size(); i++){
+            temp.push_back(nums[i]);
+            backtrack(result, nums, i+1, temp);
+            temp.pop_back();
+        }
+    }
 
     vector<vector<int>> subsets(vector<int>& nums) {
         // Recursion TC:- O(n*2^n)
@@ -29,24 +29,10 @@ public:
         // return result;
 
         // Backtracking TC:- O(n*2^n)
-    
         // this approach is quite similar to recursion just difference in recursion tree made
-        // vector<vector<int>> result;
-        // vector<int> temp;
-        // backtrack(result, nums, 0, temp);
-        // return result;
-
-        // bit manipulation (bit masking) TC:- O(n*2^n)
-        // There is no recursion stack space
         vector<vector<int>> result;
-
-        for(int mask=0; mask<(1<<nums.size()); mask++){
-            vector<int> temp;
-            for(int i=0; i<nums.size(); i++){
-                if(mask & (1<<i)) temp.push_back(nums[i]);
-            }
-            result.push_back(temp);
-        }
+        vector<int> temp;
+        backtrack(result, nums, 0, temp);
         return result;
     }
 };
