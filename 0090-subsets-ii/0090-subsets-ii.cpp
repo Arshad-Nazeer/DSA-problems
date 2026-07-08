1ce6fcbd1ad1ea2c7ad362a3bf0a5cc33a46f34f
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-07-08 19:36:05 +0530

    Time: 0 ms (100%), Space: 10.5 MB (67.76%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -10,18 +10,18 @@ public:
     //     }
     // }
 
-    void recur(vector<vector<int>>& result, vector<int>& nums, int idx, vector<int>& temp){
-        if(idx==nums.size()) {
-            result.push_back(temp);
-            return;
-        }
-        temp.push_back(nums[idx]);
-        recur(result, nums, idx+1, temp);
-        temp.pop_back();
-        // check for repeating element before going for exclude
-        while(idx+1<nums.size()&& nums[idx]==nums[idx+1]) idx++;
-        recur(result, nums, idx+1, temp);
-    }
+    // void recur(vector<vector<int>>& result, vector<int>& nums, int idx, vector<int>& temp){
+    //     if(idx==nums.size()) {
+    //         result.push_back(temp);
+    //         return;
+    //     }
+    //     temp.push_back(nums[idx]);
+    //     recur(result, nums, idx+1, temp);
+    //     temp.pop_back();
+    //     // check for repeating element before going for exclude
+    //     while(idx+1<nums.size() && nums[idx]==nums[idx+1]) idx++;
+    //     recur(result, nums, idx+1, temp);
+    // }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         // //backtrack TC:-O(n*k)=no. of unqiue subsets, worst case:- O(n*2^n)
@@ -31,11 +31,32 @@ public:
         // backtrack(result, nums, 0, temp);
         // return result;
 
-        //recursion
+        // //recursion TC:-O(n*k)=no. of unqiue subsets, worst case:- O(n*2^n)
+        // sort(nums.begin(), nums.end());
+        // vector<vector<int>> result;
+        // vector<int> temp;
+        // recur(result, nums, 0, temp);
+        // return result;
+
+        //bitmask (bit manipulation)
+        // Reject any mask that selects a duplicate element without selecting its previous occurrence.
+        // A duplicate can be selected only if its previous duplicate is also selected.
         sort(nums.begin(), nums.end());
         vector<vector<int>> result;
-        vector<int> temp;
-        recur(result, nums, 0, temp);
+        for(int mask=0; mask<(1<<nums.size()); mask++){
+            vector<int> temp;
+            bool valid=true;
+            for(int i=0; i<nums.size(); i++){
+                if(mask & (1<<i)){
+                    if(i>0 && nums[i]==nums[i-1] && !(mask&(1<<(i-1)))){
+                        valid=false;
+                        break;
+                    }
+                    temp.push_back(nums[i]);
+                }
+            }
+            if(valid) result.push_back(temp);
+        }
         return result;
     }
 };
