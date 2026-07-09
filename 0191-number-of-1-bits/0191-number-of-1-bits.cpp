16bc4868a03bc992a58a3a7bcc758abf9a053793
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-07-09 18:37:39 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.03%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        // int count=0;
+        // int count=0;  TC:-O(32)
         // for(int i=0; i<32; i++){
         //     if(n & (1<<i)) count++;
         // }
         // return count;
 
-        //shifitng n instead of 1
-        int count=0;
-        while(n){
-            count+=n&1;
-            n=n>>1;
-        }
-        return count;
+        // //shifitng n instead of 1 TC:-O(32)
+        // int count=0;
+        // while(n){
+        //     count+=n&1;
+        //     n=n>>1;
+        // }
+        // return count;
 
-        // // brian-kernighan: it removes the rightmost set bit
+        // // brian-kernighan: it removes the rightmost set bit  O(k) k: number of set bits
         // int count=0;
         // while(n){
         //     n=n&(n-1);
         //     count++;
         // }
         // return count;
+
+        //built in function
+        return __builtin_popcount(n);
     }
 };
