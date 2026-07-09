9259761fbb060c8e226f8347e4584f51c7078ee1
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-07-09 18:32:14 +0530

    Time: 0 ms (100%), Space: 8.3 MB (48.28%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     int hammingWeight(int n) {
+        // int count=0;
+        // for(int i=0; i<32; i++){
+        //     if(n & (1<<i)) count++;
+        // }
+        // return count;
+
+        // brian-kernighan: it removes the rightmost set bit
         int count=0;
-        for(int i=0; i<32; i++){
-            if(n & (1<<i)) count++;
+        while(n){
+            n=n&(n-1);
+            count++;
         }
         return count;
     }
