d6e3d4eb9a9c0a91d6a096c4ed4e277863115752
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-07-10 21:52:30 +0530

    Time: 1519 ms (5.7%), Space: 8.7 MB (38.96%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -5,10 +5,8 @@ public:
         if(dividend==INT_MIN && divisor==-1) return INT_MAX; //edge case
         int sign=1;
         if(dividend<0 ^ divisor<0) sign=-1;
-        long long dvd=dividend;  //llabs is used because abs(-2147483648) won't work
-        long long dvs=divisor;
-        dvd=llabs(dvd);
-        dvs=llabs(dvs);
+        long long dvd=llabs(dividend);  //llabs is used because abs(-2147483648) won't work
+        long long dvs=llabs(divisor);
         if(dvd == dvs) return sign;
         if(dvs == 1) return (int)(dvd*sign);   
         int count=0;
