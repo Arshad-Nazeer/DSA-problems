a418aaca94ca2aca193d6e2f9a6c7690c2b07e8f
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-07-11 21:30:44 +0530

    Time: 0 ms (100%), Space: 7.9 MB (59.55%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        // // accessing each bit of the 32 bits by rightshifitng and performing AND with 1 TC:-O(32) 
-        // int count=0;
-        // for(int i=0; i<32; i++){
-        //     if(((start>>i)&1)^((goal>>i)&1)) count++;
-        // }
-        // return count;
-
-        //brian kernighan TC:-O(different bits)
-        start=start^goal; //result contains set bits at indices different in start and goal
+        // accessing each bit of the 32 bits by rightshifitng and performing AND with 1 TC:-O(32) 
         int count=0;
-        while(start){
-            start&=(start-1);
-            count++;
+        for(int i=0; i<32; i++){
+            if(((start>>i)&1)^((goal>>i)&1)) count++;
         }
         return count;
+
+        // //brian kernighan TC:-O(different bits)
+        // start=start^goal; //result contains set bits at indices different in start and goal
+        // int count=0;
+        // while(start){
+        //     start&=(start-1);
+        //     count++;
+        // }
+        // return count;
+        
+        //builtin function
+        return __builtin_popcount(start^goal);
     }
 };
