904823ac244d8c6f3b3b81ff235d5bce47a1abc9
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-07-13 01:29:17 +0530

    Time: 49 ms (50.71%), Space: 28.9 MB (92.78%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -43,7 +43,7 @@ public:
         // result.assign(s.begin(), s.end());
         // return result;
 
-        // //brute II Aprproach
+        // //Laand II Aprproach
         // sort(nums.begin(), nums.end());
         // set<vector<int>> s;
         // vector<vector<int>> v;
@@ -59,7 +59,7 @@ public:
         // v.assign(s.begin(), s.end());
         // return v;
 
-        // //brute approach
+        // //laand approach
         // sort(nums.begin(), nums.end());
         // set<vector<int>> s;
         // vector<vector<int>> result;
