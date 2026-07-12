1958c93f3d6ffc6a5f7d598681f5793259764ec5
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-07-13 00:14:10 +0530

    Time: 0 ms (100%), Space: 13.2 MB (86.65%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        //counting set bits TC:-O(32*n)
-        int ans=0;
-        for(int i=0; i<32; i++){
-            int count=0;
-            for(int &k: nums){
-                if((k>>i)&1) count++;
-            }
-            if(count%3!=0){
-                ans=ans|(1<<i);
-            }
+        // //counting set bits TC:-O(32*n)
+        // int ans=0;
+        // for(int i=0; i<32; i++){
+        //     int count=0;
+        //     for(int &k: nums){
+        //         if((k>>i)&1) count++;
+        //     }
+        //     if(count%3!=0){
+        //         ans=ans|(1<<i);
+        //     }
+        // }
+        // return ans;
+
+        // TC:- sorting and skipping over triplets O(nlogn+n/3)
+        sort(nums.begin(), nums.end());
+        for(int i=1; i<nums.size(); i=i+3){
+            if(nums[i]!=nums[i-1]) return nums[i-1];
         }
-        return ans;
+        return nums[nums.size()-1];
     }
 };
