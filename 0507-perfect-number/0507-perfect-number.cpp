047dfbef106b81b80504a68367490573a3a9f0df
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-07-15 10:42:33 +0530

    Time: 0 ms (100%), Space: 7.7 MB (56.86%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -9,13 +9,14 @@ public:
         // return ans==num;
 
         // optimal
-        if(num==1) return false;
-        int ans=0, i;
-        for(i=1; i*i<num; i++){
-            if(num%i==0) ans+=i+num/i;
+        int ans=0;
+        for(int i=1; i*i<=num; i++){
+            if(num%i==0) {
+                ans+=i;
+                if(i!=num/i) ans+=num/i;
+            } 
         }
         ans-=num;
-        if(i*i==num) ans+=2*i;
         return ans==num;
     }
 };
