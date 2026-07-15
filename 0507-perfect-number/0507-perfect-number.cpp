876589141cd3b66a4021077f0a3d4e824cf5850e
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-07-15 10:37:48 +0530

    Time: 2 ms (60.92%), Space: 7.7 MB (90.33%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        // brute TC:-O(n)
+        // // brute TC:-O(n)
+        // int ans=0;
+        // for(int i=1; i<num; i++){
+        //     if(num%i==0) ans+=i;
+        // }
+        // return ans==num;
+
+        // optimal
         int ans=0;
-        for(int i=1; i<num; i++){
-            if(num%i==0) ans+=i;
+        for(int i=1; i*i<=num; i++){
+            if(num%i==0) {
+                ans+=i;
+                if(i!=num/i) ans+=num/i;
+            } 
         }
+        ans-=num;
         return ans==num;
     }
 };
