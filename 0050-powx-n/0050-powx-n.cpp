727ea30509fac1ea6719934933415c4dbd5edf59
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-07-16 12:45:29 +0530

    Time: 0 ms (100%), Space: 8.4 MB (93.18%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -6,15 +6,21 @@ public:
             x=1/x;
             N=-N;
         }
-
-        long double ans=1;
-        while(N!=0){
-            if(N%2!=0) {
-                ans*=x;
-            }
-            x*=x;
-            N=N/2;
+        //Non recursion:-
+        // long double ans=1;
+        // while(N!=0){
+        //     if(N%2!=0) {
+        //         ans*=x;
+        //     }
+        //     x*=x;
+        //     N=N/2;
+        // }
+        // return ans;
+        if(N==0) return 1;
+        else if(N%2!=0) return x*myPow(x, N-1);
+        else {
+            double half=myPow(x, N/2);
+            return half*half;
         }
-        return ans;
     }
 };
