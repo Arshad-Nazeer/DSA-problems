203cbd58e322e635c28e2351b6d5670a55c33376
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-07-20 19:51:41 +0530

    Time: 7 ms (98.98%), Space: 25.1 MB (68.79%) - LeetHub
    Added comments explaining the sorting approach and the frequency count method for grouping anagrams.

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // sorting approach TC:- O(n*klogk)
         vector<vector<string>> ans;
         unordered_map<string, vector<string>> mp;
         for(string s: strs){
@@ -12,5 +13,26 @@ public:
             ans.push_back(k.second);
         }
         return ans;
+
+
+        // Frequency Count + String Key TC:- O(N*K)
+        // vector<vector<string>> ans;
+        // unordered_map<string, vector<string>> mp;
+        // for(string& s: strs){
+        //     vector<int> freq(26);
+        //     string key;
+        //     for(char& c: s){
+        //         freq[c-'a']++;
+        //     }
+        //     for(int& k: freq){  //creating a unique key for anagram
+        //         key+='#';
+        //         key+=to_string(k);
+        //     }
+        //     mp[key].push_back(s);
+        // }
+        // for(auto& k: mp){
+        //     ans.push_back(move(k.second));
+        // }
+        // return ans;
     }
 };
