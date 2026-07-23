2e1741e8516485b36ca58e61e830750851bb5eb2
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-07-23 13:58:45 +0530

    Time: 0 ms (100%), Space: 9.6 MB (19.84%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        // //O(n^2)
+        // //O(k(n*m+n)) 
         // int pos=s.find(part);
         // while(pos!=string::npos){
         //     //s.replace(pos, part.size(), ""); //(starting index, no. of characters to remove, removal part)
@@ -10,13 +10,31 @@ public:
         // }
         // return s;
 
-        string ans;
+        // // TC:- O(n*m) however substring creates new string everytime
+        // string ans;
 
+        // for(char &c: s){
+        //     ans.push_back(c);
+
+        //     if(ans.size()>=part.size()){
+        //         if(ans.substr(ans.size()-part.size())==part) ans.erase(ans.size()-part.size());
+        //     }
+        // }
+        // return ans;
+
+        // TC:- O(n*m)
+        string ans;
         for(char &c: s){
             ans.push_back(c);
-
             if(ans.size()>=part.size()){
-                if(ans.substr(ans.size()-part.size())==part) ans.erase(ans.size()-part.size());
+                bool match=true;
+                for(int i=0; i<part.size(); i++){
+                    if(ans[ans.size()-part.size()+i]!=part[i]) {
+                        match=false;
+                        break;
+                    }
+                }
+                if(match) ans.erase(ans.size()-part.size());
             }
         }
         return ans;
