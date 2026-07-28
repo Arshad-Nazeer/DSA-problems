5fc21395db84cf9b13b71594f548b7ca4ebae613
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-07-28 15:44:13 +0530

    Time: 0 ms (100%), Space: 9 MB (95.68%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2){
+        //optimal approach
+        string ans;
+        int i=num1.length()-1, j=num2.length()-1, carry=0;
+        while(i>=0 || j>=0 || carry){
+            int sum=carry;
+
+            if(i>=0) sum+=num1[i--]-'0';
+            if(j>=0) sum+=num2[j--]-'0';
+
+            ans.push_back((sum%10)+'0');
+            carry=sum/10;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+
+        //laand approach
+        // string ans;
+        // int i=num1.length()-1, j=num2.length()-1, carry=0;
+        // while(i>=0 && j>=0){
+        //     int sum=(num1[i--]-'0')+(num2[j--]-'0')+carry;
+        //     ans.push_back((sum%10)+'0');
+        //     carry=sum/10;
+        // }       
+        // while(i>=0){
+        //     int sum=(num1[i--]-'0')+carry;
+        //     ans.push_back((sum%10)+'0');
+        //     carry=sum/10;
+        // }
+        // while(j>=0){
+        //     int sum=(num2[j--]-'0')+carry;
+        //     ans.push_back((sum%10)+'0');
+        //     carry=sum/10;
+        // }
+        // if(carry){
+        //     ans.append(to_string(carry));
+        // }
+        // reverse(ans.begin(), ans.end());
+        // return ans;
+    }
+};
