24900a0ecae30715614fa7915beb011796a91b83
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-08-01 15:46:39 +0530

    Refactor comments and clean up unused code
    Updated comments for clarity and removed unused code.

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        //optimal approach(two pointer)
+        // optimal approach(two pointer)
         int ans=0;
         int leftmax=height[0], rightmax=height[height.size()-1];
         int low=1, high=height.size()-2;
@@ -14,7 +14,7 @@ public:
         }
         return ans;
 
-        //prefix array approach
+        // prefix array approach
         // int n=height.size();
         // vector<int> lmax(n), rmax(n);
         // int ans=0;
@@ -31,7 +31,7 @@ public:
         // }
         // return ans;
 
-        //laand approach(TLE)
+        // Brute approach(TLE)
         // int ans=0;
         // for(int i=1; i<height.size()-1; i++){
         //     int leftmax=height[0];
@@ -45,42 +45,5 @@ public:
         //     ans+=min(leftmax, rightmax)-height[i];
         // }
         // return ans;
-
-        //tomfoolery
-        // int left, right, low, high, water=0, remove1=0, remove2=0;
-        // for(int i=0; i<height.size(); i++){
-        //     if(height[i]!=0){
-        //         left=i;
-        //         right=i+1;
-        //         break;
-        //     }
-        // }
-        // for(int i=height.size()-1; i>=0; i--){
-        //     if(height[i]!=0){
-        //         low=i-1;
-        //         high=i;
-        //         break;
-        //     }
-        // }
-        // while(right<height.size()){
-        //     if(height[right]>=height[left]){
-        //         water+=(height[left]*(right-left-1))-remove1;
-        //         left=right;
-        //         remove1=0;
-        //     }else{
-        //         if(height[right]!=0) remove1+=height[right];
-        //     }
-        //     right++;
-        // }
-        // while(low>=0){
-        //     if(height[low]>=height[high]){
-        //         water+=(height[high]*(high-low-1))-remove2;
-        //         high=low;
-        //     }else{
-        //         if(height[low]!=0) remove2+=height[low];
-        //     }
-        //     low--;
-        // }
-        // return water;
     }
 };
