0928a3376291702612b53b9ff4bc316f3cd19624
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-08-02 13:24:57 +0530

    Time: 0 ms (100%), Space: 11.7 MB (75.56%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
@@ -11,31 +11,53 @@ public:
 
 class Solution {
 public:
-    Node* dfs(Node* head){
-        Node *curr=head, *tail=head;
+    // Node* dfs(Node* head){
+    //     Node *curr=head, *tail=head;
 
-        while(curr!=NULL){
-            Node* next=curr->next;
-            if(curr->child){
-                Node* childHead=curr->child;
-                Node* childTail=dfs(childHead);
-                curr->next=childHead;
-                childHead->prev=curr;
-                curr->child=NULL;
-                childTail->next=next;
-                if(next) next->prev=childTail;
+    //     while(curr!=NULL){
+    //         Node* next=curr->next;
+    //         if(curr->child){
+    //             Node* childHead=curr->child;
+    //             Node* childTail=dfs(childHead);
+    //             curr->next=childHead;
+    //             childHead->prev=curr;
+    //             curr->child=NULL;
+    //             childTail->next=next;
+    //             if(next) next->prev=childTail;
 
-                tail=childTail;
-                curr=childTail;
-            }else tail=curr;
-            curr=curr->next;
-        }
-        return tail;
-    }
+    //             tail=childTail;
+    //             curr=childTail;
+    //         }else tail=curr; // works at the end of DLL
+    //         curr=curr->next;
+    //     }
+    //     return tail;
+    // }
 
     Node* flatten(Node* head) {
-        // recursive DFS TC:-O(n) // recursion depth SC:-O(d)
-        dfs(head);
+        // // recursive DFS TC:-O(n) // recursion depth SC:-O(d)
+        // if(!head) return NULL;
+        // dfs(head);
+        // return head;
+
+        // stack
+        if(!head) return NULL;
+        stack<Node*> stk;
+        Node *curr=head;
+        
+        while(curr){
+            if(curr->child!=NULL){
+                if(curr->next) stk.push(curr->next);
+
+                curr->next=curr->child;
+                curr->next->prev=curr;
+                curr->child=NULL;
+            }else if(curr->next==NULL && !stk.empty()){
+                curr->next=stk.top();
+                stk.pop();
+                curr->next->prev=curr;
+            }
+            curr=curr->next;
+        }
         return head;
 
         // // pointer splicing TC:- O()
