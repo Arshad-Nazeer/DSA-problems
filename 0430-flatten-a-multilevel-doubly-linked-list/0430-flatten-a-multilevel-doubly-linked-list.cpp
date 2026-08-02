03e377c5095e2a1a27379e86fabe298911f08cae
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-08-02 12:59:17 +0530

    Time: 3 ms (62.99%), Space: 11.9 MB (10.26%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
@@ -11,24 +11,50 @@ public:
 
 class Solution {
 public:
-    Node* flatten(Node* head) {
-        // pointer splicing TC:- O()
-        Node* curr=head, *temp, *child;
+    Node* dfs(Node* head){
+        Node *curr=head, *tail=head;
+
         while(curr!=NULL){
-            if(curr->child!=NULL){
-                child=curr->child;
-                temp=curr->next;
-                curr->next=child;
-                child->prev=curr;
-                while(child->next!=NULL) child=child->next;
-                child->next=temp;
-                if(temp) temp->prev=child;
+            Node* next=curr->next;
+            if(curr->child){
+                Node* childHead=curr->child;
+                Node* childTail=dfs(childHead);
+                curr->next=childHead;
+                childHead->prev=curr;
                 curr->child=NULL;
-                curr=curr->next;
-                continue;
-            }
+                childTail->next=next;
+                if(next) next->prev=childTail;
+
+                tail=childTail;
+                curr=childTail;
+            }else tail=curr;
             curr=curr->next;
         }
+        return tail;
+    }
+
+    Node* flatten(Node* head) {
+        // recursive DFS TC:-O(n) // recursion depth SC:-O(d)
+        dfs(head);
         return head;
+
+        // // pointer splicing TC:- O()
+        // Node* curr=head, *temp, *child;
+        // while(curr!=NULL){
+        //     if(curr->child!=NULL){
+        //         child=curr->child;
+        //         temp=curr->next;
+        //         curr->next=child;
+        //         child->prev=curr;
+        //         while(child->next!=NULL) child=child->next;
+        //         child->next=temp;
+        //         if(temp) temp->prev=child;
+        //         curr->child=NULL;
+        //         curr=curr->next;
+        //         continue;
+        //     }
+        //     curr=curr->next;
+        // }
+        // return head;
     }
 };
