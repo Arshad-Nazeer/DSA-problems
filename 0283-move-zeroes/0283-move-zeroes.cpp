c54ee2503bd8fe7a58b08377004c68128d688e11
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-08-09 15:32:59 +0530

    Time: 0 ms (100%), Space: 24 MB (19.87%) - LeetHub
    Removed commented-out code and unnecessary comment.

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -9,7 +9,6 @@ public:
         //     nums[i]=0;
         // }
 
-        //almost same shit
         int count=0;
         for(int i=0; i<nums.size(); i++){
             if(nums[i]!=0) swap(nums[i], nums[count++]);
