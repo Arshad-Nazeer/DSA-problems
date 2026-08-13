d7a58e95f8da689d75a866917c67db0a046316e5
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-08-13 09:12:42 +0530

    Time: 26 ms (65.53%), Space: 27.6 MB (32.17%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    // int recur(string text1, string text2, int i, int j){
+    //     if(i==text1.size() || j==text2.size()) return 0;
+    //     else if(text1[i]==text2[j]) return 1+recur(text1, text2, i+1, j+1);
+    //     else return max(recur(text1, text2, i, j+1), recur(text1, text2, i+1, j));
+    // }
+
+    // int recur(string text1, string text2, int i, int j, vector<vector<int>>& dp){
+    //     if(i==text1.size() || j==text2.size()) return 0;
+    //     else if(dp[i][j]!=-1) return dp[i][j];
+    //     else if(text1[i]==text2[j]) return dp[i][j]=1+recur(text1, text2, i+1, j+1, dp);
+    //     else return dp[i][j]=max(recur(text1, text2, i, j+1, dp), recur(text1, text2, i+1, j, dp));
+    // }
+
+    int longestCommonSubsequence(string text1, string text2) {
+        // //recursion
+        // return recur(text1, text2, 0, 0);
+
+        // //memoization O(m*n)  top-down DP
+        // vector<vector<int>> dp(text1.size(), vector<int>(text2.size(), -1));
+        // return recur(text1, text2, 0, 0, dp);
+
+        //tabulation bottom-up DP  O(m*n) better in practice than memoization due to recursion and function call overhead
+        vector<vector<int>> dp(text1.size()+1, vector<int>(text2.size()+1));
+
+        for(int i=1; i<=text1.size(); i++){
+            for(int j=1; j<=text2.size(); j++){
+                if(text1[i-1]==text2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[text1.size()][text2.size()];
+    }
+};
