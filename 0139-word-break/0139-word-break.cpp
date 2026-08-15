0a399d0b8136b0f6990513d535298607b4756a70
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-08-15 17:03:46 +0530

    Time: 0 ms (100%), Space: 10.6 MB (86%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    // bool solve(int i, string& s, unordered_set<string>& dict){
+        // if(i==s.size()) return true;
+        // if(dict.count(s.substr(i))) return true;
+        // for(int j=i; j<s.size(); j++){
+        //     if(dict.count(s.substr(i, j-i+1)))
+        //         if(solve(j+1, s, dict)) return true;
+        // }
+        // return false;
+    // }
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        // //very ass approach TC is blowing in exponential O(2^n)
+        // unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        // return solve(0, s, dict);
+        int maxlen=0;
+        for(string &str: wordDict){
+            maxlen=max(maxlen, (int)str.size());
+        }
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        vector<bool> dp(s.size()+1);
+        dp[0]=1;
+        for(int i=1; i<=s.size(); i++){
+            for(int j=i-1; j>=i-maxlen && j>=0; j--){
+                if(dp[j]==true && dict.count(s.substr(j, i-j))){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[s.size()];
+    }
+};
