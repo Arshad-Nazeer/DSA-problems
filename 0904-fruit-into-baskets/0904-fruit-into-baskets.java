a6255ffee26198289355128b618b44a4d5768dbc
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-08-23 16:28:46 +0530

    Time: 34 ms (93.23%), Space: 69.5 MB (82.07%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,6 +1,6 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        // brute force TC:-O(n^2) SC:-O(n)
+        // brute force TC:-O(n^2) SC:-O(1)
         // int ans=0, n=fruits.length;
         // for(int i=0; i<n; i++){
         //     HashSet<Integer> types = new HashSet<>();
@@ -27,7 +27,7 @@ class Solution {
         // }
         // return ans;
 
-        // sliding winodw+last occurence stored in hashmap
+        // sliding winodw+last occurence stored in hashmap, TC:- O(n), SC:- O(1)
         int left=0, right=0, ans=0;
         HashMap<Integer, Integer> mp = new HashMap<>();
         while(right<fruits.length){
@@ -42,5 +42,43 @@ class Solution {
             right++;
         }
         return ans;
+
+        // sliding window+tracking frequency in hashmap, TC:- O(n), SC:- O(1)
+        // int left=0, ans=0;
+        // HashMap<Integer, Integer> count = new HashMap<>();
+        // for(int right=0; right<fruits.length; right++){
+        //     count.put(fruits[right], count.getOrDefault(fruits[right], 0)+1);
+        //     while(count.size()>2){
+        //         count.put(fruits[left], count.get(fruits[left])-1);
+        //         if(count.get(fruits[left])==0) count.remove(fruits[left]);
+        //         left++;
+        //     }
+        //     ans=Math.max(right-left+1, ans);
+        // }
+        // return ans;
+
+        // sliding window+ optimal TC:-O(n), SC:-O(1);
+        // int left=0, ans=0, fruit1=-1, fruit2=-1, last1=-1, last2=-1;
+        // for(int right=0; right<fruits.length; right++){
+        //     if(fruit1==-1 || fruits[right]==fruit1){
+        //         fruit1=fruits[right];
+        //         last1=right;
+        //     }else if(fruit2==-1 || fruits[right]==fruit2){
+        //         fruit2=fruits[right];
+        //         last2=right;
+        //     }else{
+        //         if(last1<last2){
+        //             left=last1+1;
+        //             fruit1=fruits[right];
+        //             last1=right;
+        //         }else{
+        //             left=last2+1;
+        //             fruit2=fruits[right];
+        //             last2=right;
+        //         }
+        //     }
+        //     ans=Math.max(ans, right-left+1);
+        // }
+        // return ans;
     }
 }
