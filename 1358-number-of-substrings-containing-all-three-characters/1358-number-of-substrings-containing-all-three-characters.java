be6db61f2a3e8eabfd37fa0fe67c31df7210efb7
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-08-26 15:16:02 +0530

    Time: 14 ms (57.08%), Space: 46.3 MB (59.26%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -15,15 +15,28 @@ class Solution {
         // return count;
 
         // sliding window TC:-O(n)
-        int left=0, n=s.length(), count=0;
-        int[] freq=new int[3];
-        for(int right=0; right<n; right++){
-            freq[s.charAt(right)-'a']++;
-            while(freq[0]>0 && freq[1]>0 && freq[2]>0){
-                count+=n-right;
-                freq[s.charAt(left)-'a']--;
-                left++;
-            }
+        // int left=0, n=s.length(), count=0;
+        // int[] freq=new int[3];
+        // for(int right=0; right<n; right++){
+        //     freq[s.charAt(right)-'a']++;
+        //     while(freq[0]>0 && freq[1]>0 && freq[2]>0){
+        //         count+=n-right;
+        //         freq[s.charAt(left)-'a']--;
+        //         left++;
+        //     }
+        // }
+        // return count;
+
+        // last occurence approach TC:-O(n)
+        int count=0;
+        int[] last=new int[3];
+        last[0]=-1;
+        last[1]=-1;
+        last[2]=-1;
+        for(int i=0; i<s.length(); i++){
+            last[s.charAt(i)-'a']=i;
+            int minidx=Math.min(last[0], Math.min(last[1], last[2]));
+            if(minidx!=-1) count+=minidx+1;
         }
         return count;
     }
